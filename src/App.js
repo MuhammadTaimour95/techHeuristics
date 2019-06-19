4d87599aedaca19c2f4e822fe61e75f3a26b64a7
Muhammad Taimour <muhammad.taimour95@gmail.com>
2019-06-20 02:35:26 +0500

    Carousel

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavigationBar';
+import ControlledCarousel from './components/ControlledCarousel';
 
 function App() {
   return (
     <div className="App">
     <NavBar />
+    <ControlledCarousel />
     </div>
   );
 }
