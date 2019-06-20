7b801e289fd6fbf6f2cbf01e51338acf9258c003
Muhammad Taimour <muhammad.taimour95@gmail.com>
2019-06-20 17:19:29 +0500

    links

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 import NavBar from './components/NavigationBar';
 import ControlledCarousel from './components/ControlledCarousel';
 import Footer from './components/Footer';
+import FormPage from './components/FormPage';
+import Links from './components/Links';
 
 function App() {
   return (
     <div className="App">
     <NavBar />
     <ControlledCarousel />
+    <br>
+    </br>
+    <br>
+    </br>
+    <FormPage />
     <Footer />
     </div>
   );
